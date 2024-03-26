45f5cfd429bd27bee15917bbce47148224c175a2
volgakurvar <kurvan1112@gmail.com>
2024-03-26 21:09:59 +0900

    b

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: "steamcdn-a.akamaihd.net" }] },
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./node_modules/steamapi/**/*", "./node_modules/steamapi/*"],
+    },
+  },
 };
 
 module.exports = nextConfig;
