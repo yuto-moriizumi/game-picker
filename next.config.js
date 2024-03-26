95ba0f9eea2f954421d1df60f47a41cb853d779e
volgakurvar <kurvan1112@gmail.com>
2024-03-26 20:59:04 +0900

    remove redundant codes

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { remotePatterns: [{ hostname: "steamcdn-a.akamaihd.net" }] },
-  webpack: (config) => {
-    return {
-      ...config,
-      resolve: {
-        ...config.resolve,
-        alias: {
-          ...config.resolve.alias,
-        },
-      },
-    };
-  },
 };
 
 module.exports = nextConfig;
