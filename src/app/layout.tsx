a34b3411038ee7742274f753319a0e8ffca8a279
yuto-moriizumi <kurvan1112@gmail.com>
2025-07-08 13:41:57 +0900

    muiのキャッシュユーティリティをnext15用に更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 
 export const metadata: Metadata = {
   title: "GamePicker",
